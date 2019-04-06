6e47d6592bd2464e6f86ecae7341c2a370a1d106
redbean244 <798388771@qq.com>
2019-04-06 20:12:12 +0800

    button组件 icon组件

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,47 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- <router-view/> -->
+    <g-Button icon="setting"> 111</g-Button>
+    <g-Button icon-position="right" icon="setting"> 111</g-Button>
+    <g-Button icon="download"> 111</g-Button>
+    <g-icon name="download"></g-icon>
   </div>
 </template>
 
 <script>
+import gButton from './components/button.vue'
+import gIcon from './components/icon.vue'
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    gButton,gIcon
+  },
+  mounted() {
+    const s = document.createElement('script');
+    s.type = 'text/javascript';
+    s.src = '//at.alicdn.com/t/font_1127796_uyhosru6kk.js';
+    document.body.appendChild(s);
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {margin: 0; padding: 0; box-sizing: border-box;}
+:root {
+  --button-height: 32px;
+  --font-size: 14px;
+  --button-bg: white;
+  --button-active-bg: #eee;
+  --border-radius: 4px;
+  --color: #333;
+  --border-color: #999;
+  --border-color-hover: #666;
 }
+#app {
+  margin: 20px;
+}
+body {
+  font-size: var(--font-size);
+}
+
 </style>
---
+++ src/components/button.vue
@@ -0,0 +1,74 @@
+<template>
+  <button class="g-button" :class="{[`icon-${iconPosition}`] : true }">
+    <g-icon v-if="icon" :name="icon" class="icon"></g-icon>
+    <div class="content">
+        <slot></slot>
+    </div>
+  </button>
+</template>
+
+<script>
+import gIcon from './icon'
+
+export default {
+  name: 'g-button',  
+  props:{
+    icon:{},
+    iconPosition:{
+      type:String,
+      default: "left",
+      //属性的检查器
+      validator (value) {
+        return value === 'left' || value === 'right'
+      }
+    }
+  },
+  components:{
+    gIcon
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.g-button {
+  font-size: var(--font-size);
+  height: var(--button-height);
+  padding: 0 1em;
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
+  &:active {
+    background-color: var(--button-active-bg);
+  }
+  &:focus {
+    outline: none;
+  }
+  > .content{
+    order: 2;
+  }
+  > .icon{
+    order: 1;
+    margin-right: .3em;
+  }
+
+  &.icon-right{
+    > .content{
+      order: 1;
+    }
+    > .icon{
+      order: 2;
+      margin-left: .3em;
+      margin-right: 0;
+    }
+  }
+}
+
+</style>
