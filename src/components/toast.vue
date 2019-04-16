c9c155b8bf796b594d2359488ac7b4a3d12eaee6
redbean244 <798388771@qq.com>
2019-04-16 10:29:08 +0800

    实现了只能有一个 toast 的功能

--- src/components/toast.vue
+++ src/components/toast.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="toast">
+  <div class="toast" :class="toastClasses">
     <slot></slot>
-    <div class="line" ref="line"></div>
-    <span class="close" v-if="closeButton" @click="onClickClose">
+    <div class="line" ref="line" v-if="closeButton.text"></div>
+    <span class="close" v-if="closeButton.text" @click="onClickClose">
       {{closeButton.text}}
     </span>
   </div>
@@ -14,7 +14,7 @@
     props:{
         autoClose:{
             type:Boolean,
-            default: true
+            default: false
         },
         autoCloseDelay:{
             type:Number,
@@ -24,13 +24,27 @@
             type: Object,
             default () {
                 return {
-                    text: '关闭', callback: undefined
+                    text: '', callback: undefined
                 }
+            }
+        },
+        position: {
+            type: String,
+            default: 'top',
+            validator (value) {
+            return ['top', 'bottom', 'middle'].indexOf(value) >= 0
             }
         }
     },
     mounted(){
-        // this.execAutoClose()
+        this.execAutoClose()
+    },
+    computed: {
+      toastClasses () {
+        return {
+          [`position-${this.position}`]: true
+        }
+      }
     },
     methods:{        
         execAutoClose () {
@@ -42,6 +56,7 @@
         },
         close(){
             this.$el.remove();
+            this.$emit('close');
             this.$destroy()
         },
         onClickClose () {
@@ -59,9 +74,10 @@
   @toast-bg: rgba(0, 0, 0, 0.75);
   .toast {
     font-size: @font-size; height: @toast-height; line-height: 1.8;
-    position: fixed; top: 0; left: 50%; transform: translateX(-50%); display: flex;
+    position: fixed; display: flex;
     color: white; align-items: center; background: @toast-bg; border-radius: 4px;
     box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50); padding: 0 16px;
+    left: 50%;
     .message {
       padding: 8px 0;
     }
@@ -73,6 +89,18 @@
       height: 100%;
       border-left: 1px solid #666;
       margin-left: 16px;
+    }
+    &.position-top{
+      top: 0;
+      transform: translateX(-50%);
+    }
+    &.position-bottom{
+      bottom: 0;
+      transform: translateX(-50%);
+    }
+    &.position-middle{
+      top: 50%;
+      transform: translate(-50%, -50%);
     }
   }
 </style>
