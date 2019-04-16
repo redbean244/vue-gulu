d166f516b3d963c906848f5d17e68d3bbeaab581
redbean244 <798388771@qq.com>
2019-04-16 08:14:52 +0800

    实现 autoClose closeButton 等功能

--- src/components/toast.vue
+++ src/components/toast.vue
@@ -1,11 +1,56 @@
 <template>
   <div class="toast">
     <slot></slot>
+    <div class="line" ref="line"></div>
+    <span class="close" v-if="closeButton" @click="onClickClose">
+      {{closeButton.text}}
+    </span>
   </div>
+  
 </template>
 <script>
   export default {
-    name: 'GuluToast'
+    name: 'GuluToast',
+    props:{
+        autoClose:{
+            type:Boolean,
+            default: true
+        },
+        autoCloseDelay:{
+            type:Number,
+            default: 1000
+        },
+        closeButton: {
+            type: Object,
+            default () {
+                return {
+                    text: '关闭', callback: undefined
+                }
+            }
+        }
+    },
+    mounted(){
+        // this.execAutoClose()
+    },
+    methods:{        
+        execAutoClose () {
+            if (this.autoClose){
+                setTimeout(()=>{
+                    this.close();
+                },this.autoCloseDelay)
+            }
+        },
+        close(){
+            this.$el.remove();
+            this.$destroy()
+        },
+        onClickClose () {
+            this.close()
+            if (this.closeButton && typeof this.closeButton.callback === 'function') {
+                this.closeButton.callback(this)//this === toast实例
+            }
+        }
+    }
   }
 </script>
 <style scoped lang="less">
@@ -15,11 +60,19 @@
   .toast {
     font-size: @font-size; height: @toast-height; line-height: 1.8;
     position: fixed; top: 0; left: 50%; transform: translateX(-50%); display: flex;
-    color: white;
-    align-items: center;
-    background: @toast-bg;
-    border-radius: 4px;
-    box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50);
-    padding: 0 16px;
+    color: white; align-items: center; background: @toast-bg; border-radius: 4px;
+    box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50); padding: 0 16px;
+    .message {
+      padding: 8px 0;
+    }
+    .close {
+      padding-left: 16px;
+      flex-shrink: 0;
+    }
+    .line {
+      height: 100%;
+      border-left: 1px solid #666;
+      margin-left: 16px;
+    }
   }
 </style>
