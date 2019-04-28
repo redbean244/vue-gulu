c482b582987345f248b6bef7a0bc578959e0357c
redbean244 <798388771@qq.com>
2019-04-28 19:31:02 +0800

    添加 tabs 相关组件，添加基本 props

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,57 @@
 <template>
   <div id="app">
-      <g-button @click="showToast">中间弹出</g-button>
+        <g-tabs :selected.sync="selectedTab" >
+          <g-tabs-head>
+            <template slot="actions">
+              <button>设置</button>
+            </template>
+            <g-tabs-item name="woman">
+              <g-icon name="settings"></g-icon>美女
+            </g-tabs-item>
+            <g-tabs-item name="finance" disabled>
+              财经
+            </g-tabs-item>
+            <g-tabs-item name="sports">
+              体育
+            </g-tabs-item>
+          </g-tabs-head>
+          <g-tabs-body>
+            <g-tabs-pane name="woman">
+              美女相关资讯
+            </g-tabs-pane>
+            <g-tabs-pane name="finance">
+              财经相关资讯
+            </g-tabs-pane>
+            <g-tabs-pane name="sports">
+              体育相关资讯
+            </g-tabs-pane>
+          </g-tabs-body>
+        </g-tabs>
   </div>
 </template>
 
 <script>
 import Vue from 'vue'
-import gToast from '../../../../src/components/toast.vue'
-import gButton from '../../../../src/components/button.vue'
-import plugin from '../../../../src/assets/js/plugin.js'
-Vue.use(plugin)
+
+import gIcon from './components/icon'
+import gTabs from './components/tabs'
+import gTabsHead from './components/tabs-head'
+import gTabsBody from './components/tabs-body'
+import gTabsItem from './components/tabs-item'
+import gTabsPane from './components/tabs-pane'
+
 export default {
   name: 'App',
   components:{
-    gToast,gButton
+    gTabs,gTabsHead,gTabsBody,gTabsItem,gTabsPane,gIcon
+  },
+  data(){
+    return {
+        selectedTab: 'sports'
+    }
   },
   methods: {
-    showToast(position){
-      this.$toast('点击弹出提示', {
-        position : "middle",
-        closeButton: {
-          text: '关闭',
-          callback () {
-            console.log('关闭了')
-          }
-        },
-        autoClose: false,
-      })
-    }
+    
   }
 }
 </script>
